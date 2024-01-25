b149efad2dfce13ce8472f460d1d8791647f7f14
Suyash Lale <suyashale90@gmail.com>
2024-01-25 22:01:10 +0530

    Added more verbose mongoose schemas

--- backend/db.js
+++ backend/db.js
@@ -5,10 +5,32 @@ mongoose.connect(
 );
 
 const userSchema = mongoose.Schema({
-  username: String,
-  firstName: String,
-  lastName: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
 });
 
 // Mongoose automatically looks for the plural, lowercased version of the model name,
