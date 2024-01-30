43c38e6a8e57d22cb1c85efddbe16e28b4b4d3bd
Lale, Suyash <suyash.lale@intel.com>
2024-01-30 11:03:23 +0530

    Changed the label back

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ export function SignUp() {
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex items-center">
         <div className="bg-white rounded-lg w-80 text-center p-2 h-max px-4">
-          <Heading label={"New Sign Up"} />
+          <Heading label={"Sign Up"} />
         </div>
       </div>
     </div>
